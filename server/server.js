505fbee8d0eb3c52daa9c39eb189642ee866a336
Radha27112021 <radha.050903@gmail.com>
2024-12-31 04:19:52 +0530

    Normalize line endings

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
 import portfolioRoute from "./routes/portfolioRoutes.js";
 import dbConfig from "./config/dbConfig.js";
 
